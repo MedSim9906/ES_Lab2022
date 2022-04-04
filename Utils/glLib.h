8a69a96c427f5635b0208fa0833810dee1ac50d5
MedSim9906 <simon-mederer@gmx.de>
2022-04-04 19:20:50 +0200

    update folder structure
    https://github.com/MedSim9906/ES_Lab2022

---
+++ Utils/glLib.h
@@ -0,0 +1,19 @@
+/**
+ * EduART OS
+ * (c) ESLab, OTHR, 2022
+ */
+
+#ifndef GLLIB_H_
+#define GLLIB_H_
+
+void Beep();
+
+void Bipp();
+
+void Click();
+
+void ShortClick();
+
+void uint8_t_to_hexString(uint8_t num, char * pDest);
+
+#endif /*GLLIB_H_*/
